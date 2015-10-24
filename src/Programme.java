3d5f86baa800755f5b4373ba7fa98b84d8922a51
ALIOU1982 <thiernaliou@gmail.com>
2015-10-24 18:19:36 +0200

    Ajout de la durée

--- src/Programme.java
+++ src/Programme.java
@@ -24,6 +24,7 @@ public class Programme {
 		FileReader file = new FileReader(args[0]);
 		PageWeb p = new PageWeb();
 		List<PageWeb> pages = new ArrayList<PageWeb>();
+		long start = System.currentTimeMillis();
 		pages = p.lirefichiers(file);
 		pages = p.trieDesPagesWebParIDate(pages);
 		Navigation nav = new Navigation();
@@ -37,6 +38,7 @@ public class Programme {
 		//pv.AffichagePageVisit(visitP);
 		Map<Navigation, Float> lesProba = new HashMap<Navigation, Float>();
 		lesProba = p.probabilitesPages(mapOcc, visitP);
+		long end = System.currentTimeMillis();
 		//lesProba  = p.doSort(lesProba);
 		System.out.println("Tapez 1: Afficher le fichier, 2: Afficher les navigations par seqID\n"+
 				"\t 3: Afficher les Ocurrances des differentes pages, 4: Afficher  toutes les transiions,\n"+
@@ -52,21 +54,31 @@ public class Programme {
 		}
 		while(arret!=0){			
 			if(arret==1){
+				long debut = System.currentTimeMillis();
 				for(int i = 0; i < pages.size(); i++){
 					System.out.println(pages.get(i).getSeqID()+" à "+pages.get(i).getDatecon()+" --> "+pages.get(i).getPage());
 				}
+				System.out.println("La durée de l'exécution du programme avec l'affichage du choix est: "+ (end-start)/1000+(System.currentTimeMillis()-debut)/1000+ " secondes");
 			}
 			else if(arret==2){
+				long debut = System.currentTimeMillis();
 				nav.AffichageNavigation(navs);
+				System.out.println("La durée de l'exécution du programme avec l'affichage du choix est: "+ (end-start)/1000+(System.currentTimeMillis()-debut)/1000+ " secondes");
 			}
 			else if(arret==3){
+				long debut = System.currentTimeMillis();
 				nav.AffichagePageOcc(mapOcc);
+				System.out.println("La durée de l'exécution du programme avec l'affichage du choix est: "+ (end-start)/1000+ (System.currentTimeMillis()-debut)/1000+ " secondes");
 			}
 			else if(arret==4){
+				long debut = System.currentTimeMillis();
 				p.AffichageProbabilité(lesProba);
+				System.out.println("La durée de l'exécution du programme avec l'affichage du choix est: "+ (end-start)/1000+ (System.currentTimeMillis()-debut)/1000+ " secondes");
 			}
 			else if(arret==5){
+				long debut = System.currentTimeMillis();
 				p.AffichageBestProbabilite(lesProba, visitP);
+				System.out.println("La durée de l'exécution du programme avec l'affichage du choix est: "+ (end-start)/1000+ (System.currentTimeMillis()-debut)/1000+" secondes");
 			}
 			else{
 				System.out.println("Erreur Tapez 1: Afficher le fichier, 2: Afficher les navigations par seqID\n"+
